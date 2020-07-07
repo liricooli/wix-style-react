5d61e83328804624cc1e8b2b50cfc046ca970c1c
Dmytro Rudnytskykh <rudnitskih@gmail.com>
2020-07-07 18:26:46 +0300

    Tune perfer config

--- perfer.config.js
+++ perfer.config.js
@@ -137,7 +137,7 @@ const javascriptFiles = [
   ['VerticalTabs.bundle.min.js', 38],
   ['VerticalTabsItem.bundle.min.js', 38],
   ['WarningIndicator.bundle.min.js', 40],
-  ['HorizontalTimeline.bundle.min.js', 10],
+  ['HorizontalTimeline.bundle.min.js', 40],
 ];
 
 const cssFiles = [
