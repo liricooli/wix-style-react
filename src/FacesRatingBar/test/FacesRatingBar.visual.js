c7cdf856503d1063c75e49826b02028734a7168b
sivanharel <sivanh@wix.com>
2020-07-09 17:36:51 +0300

    implement the visual tests

--- src/FacesRatingBar/test/FacesRatingBar.visual.js
+++ src/FacesRatingBar/test/FacesRatingBar.visual.js
@@ -7,17 +7,88 @@ const commonProps = {
 };
 
 const tests = [
+  /*
   {
-    describe: 'sanity', // prop name (e.g. size)
+    describe: 'interactive',
     its: [
       {
-        it: 'default', // prop variation (e.g. small)
+        it: 'example',
+        props: {},
+      },
+    ],
+  },
+  {
+    describe: 'read only',
+    its: [
+      {
+        it: 'disapointed',
         props: {
-          // the simulation (e.g. {size: "small"})
+          value: 1,
+          readOnly: true,
+        },
+      },
+      {
+        it: 'frowning',
+        props: {
+          value: 2,
+          readOnly: true,
+        },
+      },
+      {
+        it: 'neutral',
+        props: {
+          value: 3,
+          readOnly: true,
+        },
+      },
+      {
+        it: 'smiling',
+        props: {
+          value: 4,
+          readOnly: true,
+        },
+      },
+      {
+        it: 'grining',
+        props: {
+          value: 5,
+          readOnly: true,
         },
       },
     ],
   },
+  {
+    describe: 'sizes',
+    its: [
+      {
+        it: 'small- (read only mode)',
+        props: {
+          size: 'small',
+          readOnly: true,
+        },
+      },
+      {
+        it: 'medium- (read only mode)',
+        props: {
+          size: 'medium',
+          readOnly: true,
+        },
+      },
+      {
+        it: 'small- (interactive mode)',
+        props: {
+          size: 'small',
+        },
+      },
+      {
+        it: 'medium - (interactive mode)',
+        props: {
+          size: 'medium',
+        },
+      },
+    ],
+  },
+*/
 ];
 
 tests.forEach(({ describe, its }) => {
